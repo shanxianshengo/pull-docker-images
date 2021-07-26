0cac50ca652a50671fda80fb331084c58fdf72bf
xy <xuyi@jiagouyun.com>
2021-07-26 10:09:22 +0800

    pull-docker-images

--- git-push.sh
+++ git-push.sh
@@ -4,5 +4,8 @@ git add --all
 git commit -m "pull-docker-images"
 git push -u origin master
 
+echo "travis-ci.com: "
+echo "  https://travis-ci.com/github/shanxianshengo/pull-docker-images"
 
-echo "https://travis-ci.com/github/shanxianshengo/pull-docker-images"
+echo "当前镜像列表: "
+cat images.txt |sed 's,\(.*\)/\(.*\),registry.cn-hangzhou.aliyuncs.com/googleimges/\2,g'
