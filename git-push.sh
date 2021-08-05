146b94c6d4d89814878ef89e175a89b98df4eb4e
xy <xuyi@jiagouyun.com>
2021-08-05 16:13:25 +0800

    pull-docker-images

--- git-push.sh
+++ git-push.sh
@@ -8,4 +8,8 @@ echo "travis-ci.com: "
 echo "  https://travis-ci.com/github/shanxianshengo/pull-docker-images"
 
 echo "当前镜像列表: "
+echo "docker hub: "
+cat images.txt |sed 's,\(.*\)/\(.*\),docker.io/shanbian/googleimges-\2,g'
+echo "--------------------------------------------------"
+echo "aliyun hub: "
 cat images.txt |sed 's,\(.*\)/\(.*\),registry.cn-hangzhou.aliyuncs.com/googleimges/\2,g'
